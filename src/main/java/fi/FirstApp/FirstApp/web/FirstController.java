b07473b5f19a6db2c0de160eb14c7148b6f84b84
Toni Karttunen <toni.karttunen@gmail.com>
2021-08-29 12:52:13 +0300

    First commit

---
+++ src/main/java/fi/FirstApp/FirstApp/web/FirstController.java
@@ -0,0 +1,15 @@
+package fi.FirstApp.FirstApp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FirstController {
+	@RequestMapping("hello")
+	@ResponseBody
+	public String returnGreetingForName(@RequestParam(name ="firstname", required=false, defaultValue="Käyttäjä")String name) {
+		return "Hello " + name + "!";
+	}
+}
---
+++ src/test/java/fi/FirstApp/FirstApp/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.FirstApp.FirstApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
